66f2392313dbf5fe82606d07db6a9c68fc1f91f3
Julia <juliamuniz.cm@usp.br>
2022-06-02 11:32:34 -0300

    Lalala Básica feita

--- lalala.c
+++ lalala.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
 
-	String[] frase1 = {"Azure", "Banana", "Picole"};
-	String[] frase2 = {"A", "B", "C"};
-	String[] frase3 = {"45", "23", "190"};	
+	char frase1[3][10] = {"Azure", "Banana", "Picole"};
+	char frase2[3][10] = {"A", "B", "C"};
+	char frase3[3][10]= {"45", "23", "190"};	
+	int i, j, k;
+	i = rand() % 3;
+	j = rand() % 3;
+	k = rand() % 3;
 
-
-
+	printf("%s %s %s\n", frase1[i], frase2[j], frase3[k]);
+	
+	return 0;
 }
